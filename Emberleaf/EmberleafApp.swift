c42e439f00d1d45236006eb0151647808d63ace1
Sam Pettersson <sam@sampettersson.com>
2024-10-09 12:25:06 -0700

    Initial Commit

---
+++ Emberleaf/EmberleafApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberleafApp.swift
+//  Emberleaf
+//
+//  Created by Sam Pettersson on 10/9/24.
+//
+
+import SwiftUI
+
+@main
+struct EmberleafApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmberleafTests/EmberleafTests.swift
@@ -0,0 +1,16 @@
+//
+//  EmberleafTests.swift
+//  EmberleafTests
+//
+//  Created by Sam Pettersson on 10/9/24.
+//
+
+import Testing
+
+struct EmberleafTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
